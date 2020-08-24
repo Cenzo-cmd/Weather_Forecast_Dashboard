bc4c0b722bf2f841ee6ed1f0e1872758c889c7a7
Vincent <vrdphone@gmail.com>
2020-08-24 15:31:15 -0500

    added moment.js, added current conditions, added ajax calls, click bar is functioning

--- script.js
+++ script.js
@@ -1,42 +1,93 @@
-var cities = ["Austin", "New York", "Los Angeles", "Las Vegas", "Orlando"];
+$(document).ready(function() {
+    var date = moment().format("L");
+    $("#displayDate").text(date);
 
-function renderCities() {
-    $("#addCitiesHere").empty();
-    for (var i = 0; i < cities.length; i++) {
-        var liEl = $("<li>")
-        liEl.attr("data-name", cities[i]);
-        liEl.addClass("city list-group-item");
-
-        liEl.text(cities[i]);
-        $("#addCitiesHere").append(liEl);
-        console.log(cities);
+    var cities = ["Austin", "New York", "Los Angeles", "Las Vegas", "Orlando"];
 
 
-    }
 
-    var lastCity = (cities.length - 1);
-    console.log(lastCity)
-}
-
-function updateClass() {
-    removeActive();
-    $(this).addClass("active");
-}
-
-function removeActive() {
-    for (var j = 0; j < cities.length; j++) {
-        $(".city").removeClass("active");
-    }
-}
-
-
-$("#citySubmit").on("click", function(event) {
-    event.preventDefault();
-    var newCity = $("#citySearch").val().trim();
-    cities.push(newCity);
-    console.log(newCity)
-
+    var APIKey = "dd90d41f6269d68aa74cd38310554e8a";
     renderCities();
-})
 
-$(document).on("click", ".city", updateClass);
+
+    function renderCities() {
+        $("#addCitiesHere").empty();
+        for (var i = 0; i < cities.length; i++) {
+            var liEl = $("<li>")
+            liEl.attr("data-name", cities[i]);
+            liEl.addClass("city list-group-item");
+
+            liEl.text(cities[i]);
+            $("#addCitiesHere").append(liEl);
+            // console.log(cities);
+        }
+        // firstCity = cities[0].addClass("active");
+
+
+        // console.log(lastCity)
+    }
+
+    function updateClass() {
+        removeActive();
+        $(this).addClass("active");
+
+        newCity = $(this).attr("data-name");
+        console.log(newCity);
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${APIKey}`;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+
+
+            $("#displayCityName").text(newCity);
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#temp").text("Temperature: " + response.main.temp + " degrees F");
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+            $("#uvIndex").text("UV Index: " + "")
+            console.log(response);
+
+        })
+
+    }
+
+    function removeActive() {
+        for (var j = 0; j < cities.length; j++) {
+            $(".city").removeClass("active");
+        }
+    }
+
+    $("#citySubmit").on("click", function(event) {
+        event.preventDefault();
+        var newCity = $("#citySearch").val().trim();
+        cities.unshift(newCity);
+        // console.log(newCity);
+        searchCity();
+
+        renderCities();
+    })
+
+    function searchCity(newCity) {
+        var newCity = $("#citySearch").val().trim();
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${APIKey}`;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            $("#displayCityName").text(newCity);
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#temp").text("Temperature: " + response.main.temp + " degrees F");
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+            $("#uvIndex").text("UV Index: " + "")
+            console.log(response);
+
+
+        })
+    }
+
+    $(document).on("click", ".city", updateClass);
+
+})
