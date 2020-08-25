3c89c178406d2eb4d22eed08f89d4a28c3559306
Vincent <vrdphone@gmail.com>
2020-08-25 00:05:54 -0500

    added background, finished 5 day forecast

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var date = moment().format("L");
     $("#displayDate").text(date);
 
-    var cities = ["Austin", "New York", "Los Angeles", "Las Vegas", "Orlando"];
+    var cities = ["Austin", "New York", "Los Angeles", "Las Vegas", "Seattle"];
 
     var APIKey = "dd90d41f6269d68aa74cd38310554e8a";
     renderCities();
@@ -41,6 +41,9 @@ $(document).ready(function() {
     $("#citySubmit").on("click", function(event) {
         event.preventDefault();
         var newCity = $("#citySearch").val().trim();
+        if (newCity === undefined || newCity === NaN || newCity === "") {
+            return;
+        };
         cities.unshift(newCity);
         // console.log(newCity);
         searchCity(newCity);
@@ -49,10 +52,9 @@ $(document).ready(function() {
 
     function searchCity(newCity) {
         var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${APIKey}`;
-        console.log(newCity);
-        if (newCity === undefined || newCity === NaN) {
+        if (newCity === undefined || newCity === NaN || newCity === "") {
             return;
-        }
+        };
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -77,19 +79,25 @@ $(document).ready(function() {
                 var fiveDay = result.daily;
                 $("#uvIndex").text(result.current.uvi);
                 for (var x = 1; x < 6; x++) {
-                    let fiveDayTemp = result.daily[x].temp.day;
+                    let fiveDayTemp = result.daily[x].temp.day.toFixed(1);
                     let fiveDayHumidity = result.daily[x].humidity;
                     let fiveDayDate = result.daily[x].sunrise;
+                    let fiveDayWind = result.daily[x].wind_speed.toFixed(1);
+                    let fiveDayPic = result.daily[x].weather[0].icon;
+                    let fiveDayIcon = "http://openweathermap.org/img/wn/" + fiveDayPic + ".png";
+
                     fiveDayDate = fiveDayDate * 1000;
                     // console.log(fiveDayDate);
                     let dateObject = new Date(fiveDayDate);
                     let humanDate = dateObject.toLocaleDateString();
                     // console.log(humanDate);
 
-                    let divEl = $("<div>");
-                    divEl.addClass("card fiveDayCard");
+                    let divEl = $("<div>").addClass("card fiveDayCard");;
+                    let imageEl = $("<img>").addClass("fiveDayImage").attr("src", fiveDayIcon);;
                     $(divEl).append("<h5>" + humanDate + "</h5>");
-                    $(divEl).append("<p>" + "Temp: " + fiveDayTemp + " degrees F" + "</p>");
+                    $(divEl).append(imageEl);
+                    $(divEl).append("<p>" + "Temp: " + fiveDayTemp + " F" + "</p>");
+                    $(divEl).append("<p>" + "Wind " + fiveDayWind + " MPH" + "</p>");
                     $(divEl).append("<p>" + "Humidity: " + fiveDayHumidity + "%" + "</p>");
 
 
