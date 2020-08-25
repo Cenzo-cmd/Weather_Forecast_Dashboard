7afd68d41d4aad5007e859a60f898d4aa2216a0b
Vincent <vrdphone@gmail.com>
2020-08-24 23:11:12 -0500

    added 5 day forecast items, fixed loading bug that throws error in console

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ $(document).ready(function() {
 
     var cities = ["Austin", "New York", "Los Angeles", "Las Vegas", "Orlando"];
 
-
-
     var APIKey = "dd90d41f6269d68aa74cd38310554e8a";
     renderCities();
+    updateClass();
 
 
     function renderCities() {
@@ -16,41 +15,21 @@ $(document).ready(function() {
             var liEl = $("<li>")
             liEl.attr("data-name", cities[i]);
             liEl.addClass("city list-group-item");
-
+            if (i === 0) {
+                liEl.addClass("active");
+            }
             liEl.text(cities[i]);
             $("#addCitiesHere").append(liEl);
-            // console.log(cities);
+
         }
-        // firstCity = cities[0].addClass("active");
-
-
-        // console.log(lastCity)
     }
 
     function updateClass() {
         removeActive();
         $(this).addClass("active");
-
         newCity = $(this).attr("data-name");
-        console.log(newCity);
-        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${APIKey}`;
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-
-
-
-            $("#displayCityName").text(newCity);
-            $("#humidity").text("Humidity: " + response.main.humidity + "%");
-            $("#temp").text("Temperature: " + response.main.temp + " degrees F");
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
-            $("#uvIndex").text("UV Index: " + "")
-            console.log(response);
-
-        })
-
+        // console.log(newCity);
+        searchCity(newCity);
     }
 
     function removeActive() {
@@ -64,19 +43,21 @@ $(document).ready(function() {
         var newCity = $("#citySearch").val().trim();
         cities.unshift(newCity);
         // console.log(newCity);
-        searchCity();
-
+        searchCity(newCity);
         renderCities();
     })
 
     function searchCity(newCity) {
-        var newCity = $("#citySearch").val().trim();
         var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${APIKey}`;
-
+        console.log(newCity);
+        if (newCity === undefined || newCity === NaN) {
+            return;
+        }
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
+
             $("#displayCityName").text(newCity);
             $("#humidity").text("Humidity: " + response.main.humidity + "%");
             $("#temp").text("Temperature: " + response.main.temp + " degrees F");
@@ -84,8 +65,43 @@ $(document).ready(function() {
             $("#uvIndex").text("UV Index: " + "")
             console.log(response);
 
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var latLong = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
+            $.ajax({
+                url: latLong,
+                method: "GET"
+            }).then(function(result) {
+                console.log(result);
+                $("#forecast").empty();
+                var fiveDay = result.daily;
+                $("#uvIndex").text(result.current.uvi);
+                for (var x = 1; x < 6; x++) {
+                    let fiveDayTemp = result.daily[x].temp.day;
+                    let fiveDayHumidity = result.daily[x].humidity;
+                    let fiveDayDate = result.daily[x].sunrise;
+                    fiveDayDate = fiveDayDate * 1000;
+                    // console.log(fiveDayDate);
+                    let dateObject = new Date(fiveDayDate);
+                    let humanDate = dateObject.toLocaleDateString();
+                    // console.log(humanDate);
+
+                    let divEl = $("<div>");
+                    divEl.addClass("card fiveDayCard");
+                    $(divEl).append("<h5>" + humanDate + "</h5>");
+                    $(divEl).append("<p>" + "Temp: " + fiveDayTemp + " degrees F" + "</p>");
+                    $(divEl).append("<p>" + "Humidity: " + fiveDayHumidity + "%" + "</p>");
+
+
+                    $("#forecast").append(divEl);
+
+
+                }
+
+            })
 
         })
+
     }
 
     $(document).on("click", ".city", updateClass);
